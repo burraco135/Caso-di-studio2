79049121cad2f0804142cd1c8f9d99a3a45a31d8
burraco135 <estermolinari135@gmail.com>
2020-05-08 14:37:39 +0000

    utente+risorse

--- Menu/menu.c
+++ Menu/menu.c
@@ -1,7 +1,30 @@
 #include "menu.h"
-#include "gestione.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+
+#define MAX_CH 20
+
+typedef struct {
+  char nome[MAX_CH];
+  char cognome[MAX_CH];
+  int disciplina;
+  int livello;
+} utente_t;
+
+typedef struct {
+  char titolo[MAX_CH];
+	int livello;
+	int disciplina;
+	char autore[MAX_CH];
+  char parola_chiave[MAX_CH];  
+  int valutazione;
+  int tipologia;
+} risorsa_t;
+
+void CreaUtente(FILE *fileUtente);
+void CreaRisorse(FILE *fileRisorse);
+void EliminaUtente(FILE *fileUtente);
 
 void menu_iniziale(int opzione) { 
 	int status = 0; // Controlla l'input
@@ -45,7 +68,7 @@ void menu_iniziale(int opzione) {
 
 void menu_utilizzatore(int opzione) {
 	int status = 0, accesso = 0;
-	char nome_utente[10] = "";
+	char nome_utente[MAX_CH] = "";
 	int password = 0;
 
 	puts("Inserisci nome utente e password.");
@@ -82,6 +105,7 @@ void menu_utilizzatore(int opzione) {
 } // Fine menu_utilizzatore
 
 void menu_creatore(int opzione) {
+  FILE *fRisorse;
 	int status = 0, accesso = 0;
 	char nome_utente[10] = "";
 	int password = 0;
@@ -93,6 +117,7 @@ void menu_creatore(int opzione) {
 			switch (opzione) {
 				case 1:
 					puts("> Gestione risorse...");
+					CreaRisorse(fRisorse);
 					break;
 				case 2:
 					puts("> Lettura statistiche...");
@@ -125,13 +150,16 @@ void menu_amministratore(int opzione) {
 			switch(opzione) {
 				case 1:
 					puts("> Elimina utente...");
+					EliminaUtente(fUtente);
+					// TODO
 					break;
 				case 2:
 					puts("> Modifica utente...");
 					break;
 				case 3:
 					puts("> Crea utente...");
-					CreaFileUtente(fUtente);
+					CreaUtente(fUtente);    
+          puts("\nPremi [1] per eliminare un utente\nPremi [2] per modificare un utente\nPremi [3] per creare un utente\nPremi [4] per disconnetterti");       				
 					break;
 				case 4:
 				 	puts("> Disconnessione in corso...");
@@ -144,3 +172,47 @@ void menu_amministratore(int opzione) {
 		} // Fine while
 	} // Fine if
 } // Fine menu_amministratore
+
+void CreaUtente(FILE *fileUtente){
+  utente_t utente = {"","",0,0};
+  if((fileUtente = fopen("FileUtente.txt","a+"))==NULL) {
+    puts("File corrotto o inesistente.");  
+  } else {
+    puts("Inserisci: Nome, Cognome, Disciplina e Livello");
+    puts("Premi CTRL-Z per terminare.");
+		printf("%s", "? ");
+    scanf("%s %s %d %d", utente.nome, utente.cognome, &utente.disciplina, &utente.livello);
+
+		while(!feof(stdin)){   
+			
+      fprintf(fileUtente,"%s %s %d %d\n", utente.nome, utente.cognome, utente.disciplina, utente.livello);
+      printf("%s", "? ");
+      scanf("%s %s %d %d", utente.nome, utente.cognome, &utente.disciplina, &utente.livello);      
+    } 
+  fclose(fileUtente);
+  }
+}
+
+void CreaRisorse(FILE *fileRisorse){
+  risorsa_t risorsa = {"", 0, 0, "", "", 0, 0};
+  if((fileRisorse = fopen("FileRisorse.txt","a+"))==NULL) {
+    puts("File corrotto o inesistente.");  
+  } else {
+    puts("Inserisci : Titolo, livello educativo, disciplina,\nautore, tipologia ");
+    puts("Premi CTRL-Z per terminare.");
+		printf("%s", "? ");
+    scanf("%20s %d %d %s %d", risorsa.titolo, &risorsa.livello, &risorsa.disciplina, risorsa.autore, &risorsa.tipologia);
+
+		while(!feof(stdin)){   
+			
+      fprintf(fileRisorse,"%s %d %d %s %d\n", risorsa.titolo, risorsa.livello, risorsa.disciplina, risorsa.autore, risorsa.tipologia);
+      printf("%s", "? ");
+      scanf("%s %d %d %s %d", risorsa.titolo, &risorsa.livello, &risorsa.disciplina, risorsa.autore, &risorsa.tipologia);
+    } 
+  fclose(fileRisorse);
+  }
+}
+
+void EliminaUtente(FILE *fileUtente) {
+
+}
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "menu.h"
-#include "gestione.h"
 
 //prototipi funzioni
 //int accedi(char nome[], int password);
