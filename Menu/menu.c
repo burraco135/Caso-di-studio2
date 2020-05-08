95121e4253d9ea1ef42b959f49e78c111d45b74f
burraco135 <estermolinari135@gmail.com>
2020-05-08 17:18:49 +0000

    elimina utente

--- Menu/menu.c
+++ Menu/menu.c
@@ -1,3 +1,4 @@
+
 #include "menu.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,6 +7,7 @@
 #define MAX_CH 20
 
 typedef struct {
+	int codice;
   char nome[MAX_CH];
   char cognome[MAX_CH];
   int disciplina;
@@ -174,35 +176,47 @@ void menu_amministratore(int opzione) {
 } // Fine menu_amministratore
 
 void CreaUtente(FILE *fileUtente){
-  utente_t utente = {"","",0,0};
-  if((fileUtente = fopen("FileUtente.txt","a+"))==NULL) {
+  utente_t utente = {0, "", "", 0, 0};
+  if((fileUtente = fopen("FileUtente.dat","wb"))==NULL) {
     puts("File corrotto o inesistente.");  
   } else {
-    puts("Inserisci: Nome, Cognome, Disciplina e Livello");
+		puts("Inserisci: Nome, Cognome, Disciplina e Livello");
     puts("Premi CTRL-Z per terminare.");
-		printf("%s", "? ");
-    scanf("%s %s %d %d", utente.nome, utente.cognome, &utente.disciplina, &utente.livello);
-
-		while(!feof(stdin)){   
-			
-      fprintf(fileUtente,"%s %s %d %d\n", utente.nome, utente.cognome, utente.disciplina, utente.livello);
-      printf("%s", "? ");
-      scanf("%s %s %d %d", utente.nome, utente.cognome, &utente.disciplina, &utente.livello);      
+		while(!feof(stdin)){
+			printf("%s", "? ");
+			fscanf(stdin,"%19s%19s%d%d",utente.nome,utente.cognome,&utente.disciplina,&utente.livello);
+			utente.codice++;     
+      fwrite(&utente, sizeof(utente_t), 1, fileUtente);
     } 
   fclose(fileUtente);
   }
+
+	// stampa
+	if ((fileUtente = fopen("FileUtente.dat", "rb")) == NULL) {
+		puts("Meh mo basta");
+	} else {
+		puts("\nCodice\tNome\tCognome\tDisciplina\tLivello");
+		while (!feof(fileUtente)) {
+			utente_t utente = {0, "", "", 0, 0};
+			int result = fread(&utente, sizeof(utente_t), 1, fileUtente);
+
+			if (result != 0 ) {
+				printf("%d\t%s\t%s\t%d\t%d\n", utente.codice, utente.nome, utente.cognome, utente.disciplina, utente.livello);
+			}
+		}
+		fclose(fileUtente);
+	}
 }
 
 void CreaRisorse(FILE *fileRisorse){
   risorsa_t risorsa = {"", 0, 0, "", "", 0, 0};
-  if((fileRisorse = fopen("FileRisorse.txt","a+"))==NULL) {
+  if((fileRisorse = fopen("FileRisorse.txt","r+"))==NULL) {
     puts("File corrotto o inesistente.");  
   } else {
     puts("Inserisci : Titolo, livello educativo, disciplina,\nautore, tipologia ");
     puts("Premi CTRL-Z per terminare.");
 		printf("%s", "? ");
-    scanf("%20s %d %d %s %d", risorsa.titolo, &risorsa.livello, &risorsa.disciplina, risorsa.autore, &risorsa.tipologia);
-
+    scanf("%s %d %d %s %d", risorsa.titolo, &risorsa.livello, &risorsa.disciplina, risorsa.autore, &risorsa.tipologia);
 		while(!feof(stdin)){   
 			
       fprintf(fileRisorse,"%s %d %d %s %d\n", risorsa.titolo, risorsa.livello, risorsa.disciplina, risorsa.autore, risorsa.tipologia);
@@ -214,5 +228,30 @@ void CreaRisorse(FILE *fileRisorse){
 }
 
 void EliminaUtente(FILE *fileUtente) {
+	if ((fileUtente = fopen("FileUtente.dat", "rb")) == NULL) {
+		puts("Meh mo basta");
+	} else {
+		puts("\nCodice\tNome\tCognome\tDisciplina\tLivello");
+		while (!feof(fileUtente)) {
+			utente_t utente = {0, "", "", 0, 0};
+			int result = fread(&utente, sizeof(utente_t), 1, fileUtente);
 
+			if (result != 0 ) {
+				printf("%d\t%s\t%s\t%d\t%d\n", utente.codice, utente.nome, utente.cognome, utente.disciplina, utente.livello);
+			}
+		}
+		int elimina = 0;
+		puts("\nScegli l'utente da eliminare: ");
+		scanf("%d", &elimina);
+
+      fseek(fileUtente, (elimina - 1) * sizeof(utente_t), SEEK_SET); // i = 0
+      utente_t utente;
+    	fread(&utente,sizeof(utente_t),1,fileUtente);
+    	
+    		fseek(fileUtente,(elimina - 1)*sizeof(utente_t), SEEK_SET);
+    		utente_t utente_vuoto = {0, "", "", 0, 0};
+    		fwrite(&utente_vuoto,sizeof(utente_t),1,fileUtente);
+				puts("Sono qui");
+		fclose(fileUtente);
+  }
 }
